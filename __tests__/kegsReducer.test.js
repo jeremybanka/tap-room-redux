07cea57565fccca3a629d4a86b6a4e6c424d5a56
Jeremy Banka <hello@jeremybanka.com>
2021-07-12 00:30:21 -0700

    ✅ add further tests; all actions now tested

--- __tests__/kegsReducer.test.js
+++ __tests__/kegsReducer.test.js
@@ -1,40 +1,31 @@
 import { specToHex } from "luum"
-import kegsReducer, { create } from "../src/store/kegs"
+import kegsReducer, { create, decrement } from "../src/store/kegs"
 import initialState from "../src/store/kegs/initialState"
 
-describe(`kegsReducer`, () => {
-  it(`can create a new keg without a name`, () => {
+const colorFromSecondsHand = () => specToHex({
+  hue: new Date().getSeconds() * 6,
+  sat: 200,
+  lum: 0.7,
+  prefer: `lum`,
+})
+
+describe(`kegs/create`, () => {
+  it(`can create a new keg with default values`, () => {
     const { type } = create
-    const action = { type, payload: {} }
+    const action = { type,
+      payload: {
+        name: undefined,
+        brand: undefined,
+        price: undefined,
+        flavor: undefined,
+      } }
     const newKegs = kegsReducer(
       initialState,
       action
     )
-    const expectedColor = specToHex({
-      hue: new Date().getSeconds() * 6,
-      sat: 200,
-      lum: 0.7,
-      prefer: `lum`,
-    })
+    const expectedColor = colorFromSecondsHand()
     const expected = [
-      {
-        name: `Effervescent Brew`,
-        brand: `Generic Brand`,
-        price: 35,
-        flavor: `Foamy Flavor`,
-        color: `#0a9`,
-        remaining: 20,
-        total: 124,
-      },
-      {
-        name: `Celestial Brew`,
-        brand: `Name Brand`,
-        price: 53,
-        flavor: `Savory Flavor`,
-        color: `#2af`,
-        remaining: 56,
-        total: 124,
-      },
+      ...initialState,
       {
         name: `Unidentified Brew`,
         brand: `Generic Brand`,
@@ -48,5 +39,49 @@ describe(`kegsReducer`, () => {
     newKegs.forEach((keg, idx) => {
       expect(keg).toEqual(expected[idx])
     })
+  })
+  it(`can create a new keg with specified values`, () => {
+    const { type } = create
+    const action = { type,
+      payload: {
+        name: `Pirate`,
+        brand: `Outlaw`,
+        price: 562,
+        flavor: `Acidic`,
+      } }
+    const newKegs = kegsReducer(
+      initialState,
+      action
+    )
+    const expectedColor = colorFromSecondsHand()
+    const expected = [
+      ...initialState,
+      {
+        name: `Pirate Brew`,
+        brand: `Outlaw Brand`,
+        price: 562,
+        flavor: `Acidic Flavor`,
+        color: expectedColor,
+        total: 124,
+        remaining: 124,
+      },
+    ]
+    newKegs.forEach((keg, idx) => {
+      expect(keg).toEqual(expected[idx])
+    })
+  })
+})
+
+describe(`kegs/decrement`, () => {
+  it(`takes one away from the keg at the specified index`, () => {
+    const targetIdx = 0
+    const targetKegInitial = initialState[targetIdx]
+    const { type } = decrement
+    const payload = targetIdx
+    const action = { type, payload }
+    const newState = kegsReducer(initialState, action)
+    const targetKegNew = newState[targetIdx]
+    expect(targetKegNew.remaining)
+      .toBe(targetKegInitial.remaining - 1)
   })
 })
--- __tests__/newKegReducer.test.js
+++ __tests__/newKegReducer.test.js
@@ -1,10 +1,72 @@
-import newKegReducer, { initialState, toggleOpen } from "../src/store/newKeg"
+import newKegReducer, {
+  edit,
+  initialState,
+  reset,
+  toggleOpen,
+} from "../src/store/newKeg"
 
-describe(`new Keg Reducer`, () => {
-  it(`can toggle the open state of the newKeg interface`, () => {
+describe(`newKeg/toggleOpen`, () => {
+  it(`toggles the open state of the newKeg slice`, () => {
     const { type } = toggleOpen
     const action = { type }
     const newState = newKegReducer(initialState, action)
     expect(newState.isOpen).toBe(!initialState.isOpen)
+  })
+})
+
+describe(`newKeg/edit`, () => {
+  it(`can change the name of the new keg`, () => {
+    const { type } = edit
+    const payload = {
+      key: `name`,
+      value: `Orange`,
+    }
+    const action = { type, payload }
+    const newState = newKegReducer(initialState, action)
+    expect(newState.name).toBe(`Orange`)
+  })
+  it(`can change the brand of the new keg`, () => {
+    const { type } = edit
+    const payload = {
+      key: `brand`,
+      value: `Fancy`,
+    }
+    const action = { type, payload }
+    const newState = newKegReducer(initialState, action)
+    expect(newState.brand).toBe(`Fancy`)
+  })
+  it(`can change the price of the new keg`, () => {
+    const { type } = edit
+    const payload = {
+      key: `price`,
+      value: 23,
+    }
+    const action = { type, payload }
+    const newState = newKegReducer(initialState, action)
+    expect(newState.price).toBe(23)
+  })
+  it(`can change the flavor of the new keg`, () => {
+    const { type } = edit
+    const payload = {
+      key: `flavor`,
+      value: `Bitter`,
+    }
+    const action = { type, payload }
+    const newState = newKegReducer(initialState, action)
+    expect(newState.flavor).toBe(`Bitter`)
+  })
+})
+
+describe(`newKeg/reset`, () => {
+  it(`resets new keg to defaults`, () => {
+    const { type } = edit
+    const payload = {
+      key: `name`,
+      value: `Orange`,
+    }
+    const action = { type, payload }
+    const newState = newKegReducer(initialState, action)
+    const resetState = newKegReducer(newState, { type: reset })
+    expect(resetState.name).toBe(``)
   })
 })
